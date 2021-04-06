39fdb0a12a5a5e8aa317da80b87465161f109460
Angiela Moreno <angielamoreno@MacBook-Pro-de-Angiela.local>
2021-04-06 12:30:16 -0500

    js added to the project

---
+++ animate.js
@@ -0,0 +1,5 @@
+let btnOpen = document.querySelector('button')
+
+btnOpen.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=2p3zZoraK9g&ab_channel=SZAVEVO",'_blank');
+})
